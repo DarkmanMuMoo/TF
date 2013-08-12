6ea08a067f87f7d9cad403637f01d516fd162770
MuMoo <darkmanmumoonaja@gmail.com>
2013-08-12 21:49:02 +0700

    dsfg

---
+++ application/logs/log-2013-07-23.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-07-23 16:34:24 --> 404 Page Not Found --> tf
+ERROR - 2013-07-23 16:35:07 --> 404 Page Not Found --> tf
